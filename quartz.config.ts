dccefab269e8adde5fc4b66e9399917af8b15539
Zebra Linux <q1033416242@gmail.com>
2024-12-07 15:43:35 +0800

    config changed: 变更baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "",
+    baseUrl: "sockingpanda.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
